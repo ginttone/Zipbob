d91393849acb28120a73359cbadfd930feeb7c5f
juni <alicelikesbab@gmail.com>
2021-09-23 17:01:22 +0900

    mid commit3

--- home/views.py
+++ home/views.py
@@ -15,32 +15,44 @@ def frame_test(request):
     result={}
     return render(request, 'frame_test.html', context=result)
 
-def list(request):
-    result = dict()
-    conn = sqlite3.connect('db.sqlite3')
-    conn.row_factory = sqlite3.Row  # for getting columns
-    curs = conn.cursor()
-    # economics
-    curs.execute('select * from recipe_tot_youtube')
-    data = curs.fetchall()
-    for row in data:
-        print(row['title'], ' : ', row['method'])
-    result['erows'] = data
-    return render(request, 'premium.html', result)
+
+
+# def list(request):
+#     result = dict()
+#     conn = sqlite3.connect('../db.sqlite3')
+#     conn.row_factory = sqlite3.Row  # for getting columns
+#     curs = conn.cursor()
+#     # economics
+#     curs.execute('select * from recipe_tot_youtube')
+#     data = curs.fetchall()
+#     for row in data:
+#         print(row['title'], ' : ', row['method'])
+#         # result['erows'] = data
+#     return render(request, 'frame_test.html', result)
+
+# def list_paginator(request):
+#     conn = sqlite3.connect('../db.sqlite3')
+#     conn.row_factory = sqlite3.Row  # for getting columns
+#     curs = conn.cursor()
+#     # economics
+#     curs.execute('select * from recipe_tot_youtube')
+#     data = curs.fetchall()
+#     for row in data:
+#         print(row['title'], row['method'])
+#     paginator = Paginator(data, 5)
+#     result = dict()
+#     result['paginator'] = paginator
+#     # request.GET['page']
+#     page_number = request.GET.get('page', 1)
+#     result['page_obj'] = paginator.get_page(page_number)
+#     return render(request, 'frame_test.html', context=result)
 
 def list_paginator(request):
-    conn = sqlite3.connect('db.sqlite3')
-    conn.row_factory = sqlite3.Row  # for getting columns
-    curs = conn.cursor()
-    # economics
-    curs.execute('select * from recipe_tot_youtube')
+    conn = sqlite3.connect('../db.sqlite3')
+    conn.row_factory = sqlite3.Row
+    curs = conn.cursor() #명령어 잡아오는 아이
+    curs.execute('select*from polls_economics pe')
     data = curs.fetchall()
     for row in data:
-        print(row['title'], row['method'])
-    paginator = Paginator(data, 5)
-    result = dict()
-    result['paginator'] = paginator
-    # request.GET['page']
-    page_number = request.GET.get('page', 1)
-    result['page_obj'] = paginator.get_page(page_number)
-    return render(request, 'premium.html', context=result)
+        print(row['title'],row['href'])
+    return 0
