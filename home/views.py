116759f4b84e82e271944ef8de371760fa7070b5
juni <alicelikesbab@gmail.com>
2021-09-23 14:31:42 +0900

    mid commit2

--- home/views.py
+++ home/views.py
@@ -15,6 +15,19 @@ def frame_test(request):
     result={}
     return render(request, 'frame_test.html', context=result)
 
+def list(request):
+    result = dict()
+    conn = sqlite3.connect('db.sqlite3')
+    conn.row_factory = sqlite3.Row  # for getting columns
+    curs = conn.cursor()
+    # economics
+    curs.execute('select * from recipe_tot_youtube')
+    data = curs.fetchall()
+    for row in data:
+        print(row['title'], ' : ', row['method'])
+    result['erows'] = data
+    return render(request, 'premium.html', result)
+
 def list_paginator(request):
     conn = sqlite3.connect('db.sqlite3')
     conn.row_factory = sqlite3.Row  # for getting columns
@@ -23,11 +36,11 @@ def list_paginator(request):
     curs.execute('select * from recipe_tot_youtube')
     data = curs.fetchall()
     for row in data:
-        print(row['title'], row['ingredients'])
+        print(row['title'], row['method'])
     paginator = Paginator(data, 5)
     result = dict()
     result['paginator'] = paginator
     # request.GET['page']
     page_number = request.GET.get('page', 1)
     result['page_obj'] = paginator.get_page(page_number)
-    return render(request, 'frame_test.html', context=result)
+    return render(request, 'premium.html', context=result)
