76afb4d8a5fbc5601e1ebc485e616c7e99e88b48
juni <alicelikesbab@gmail.com>
2021-09-23 14:03:17 +0900

    mid commit

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
+import sqlite3
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
@@ -11,4 +13,21 @@ def premium(request):
 
 def frame_test(request):
     result={}
+    return render(request, 'frame_test.html', context=result)
+
+def list_paginator(request):
+    conn = sqlite3.connect('db.sqlite3')
+    conn.row_factory = sqlite3.Row  # for getting columns
+    curs = conn.cursor()
+    # economics
+    curs.execute('select * from recipe_tot_youtube')
+    data = curs.fetchall()
+    for row in data:
+        print(row['title'], row['ingredients'])
+    paginator = Paginator(data, 5)
+    result = dict()
+    result['paginator'] = paginator
+    # request.GET['page']
+    page_number = request.GET.get('page', 1)
+    result['page_obj'] = paginator.get_page(page_number)
     return render(request, 'frame_test.html', context=result)
