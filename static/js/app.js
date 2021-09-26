13ba24fa1e2f7ef91cfed7b431399bd4fd0719eb
juni <alicelikesbab@gmail.com>
2021-09-27 00:49:04 +0900

    commit1

--- static/js/app.js
+++ static/js/app.js
@@ -121,7 +121,7 @@ jQuery(document).ready(function($){
             },
 
             title: {
-                text: 'Most common desktop screen readers'
+                text: '농수산물 물가 시세 추이'
             },
 
             subtitle: {
@@ -130,7 +130,7 @@ jQuery(document).ready(function($){
 
             yAxis: {
                 title: {
-                    text: 'Percentage usage'
+                    text: '세로축'
                 },
                 accessibility: {
                     description: 'Percentage usage'
@@ -139,12 +139,12 @@ jQuery(document).ready(function($){
 
             xAxis: {
                 title: {
-                    text: 'Time'
+                    text: '가로 축'
                 },
                 accessibility: {
-                    description: 'Time from December 2010 to September 2019'
+                    description: '몇일부터 몇일 사이 '
                 },
-                categories: ['December 2010', 'May 2012', 'January 2014', 'July 2015', 'October 2017', 'September 2019']
+                categories: ['1주', '2주', '3주', '4주', '오늘', '내일']
             },
 
             tooltip: {
@@ -168,39 +168,33 @@ jQuery(document).ready(function($){
                 {
                     name: 'NVDA',
                     data: [34.8, 43.0, 51.2, 41.4, 64.9, 72.4],
-                    website: 'https://www.nvaccess.org',
                     color: colors[2],
                     accessibility: {
-                        description: 'This is the most used screen reader in 2019'
+                        description: '설명'
                     }
                 }, {
                     name: 'JAWS',
                     data: [69.6, 63.7, 63.9, 43.7, 66.0, 61.7],
-                    website: 'https://www.freedomscientific.com/Products/Blindness/JAWS',
                     dashStyle: 'ShortDashDot',
                     color: colors[0]
                 }, {
                     name: 'VoiceOver',
                     data: [20.2, 30.7, 36.8, 30.9, 39.6, 47.1],
-                    website: 'http://www.apple.com/accessibility/osx/voiceover',
                     dashStyle: 'ShortDot',
                     color: colors[1]
                 }, {
                     name: 'Narrator',
                     data: [null, null, null, null, 21.4, 30.3],
-                    website: 'https://support.microsoft.com/en-us/help/22798/windows-10-complete-guide-to-narrator',
                     dashStyle: 'Dash',
                     color: colors[9]
                 }, {
                     name: 'ZoomText/Fusion',
                     data: [6.1, 6.8, 5.3, 27.5, 6.0, 5.5],
-                    website: 'http://www.zoomtext.com/products/zoomtext-magnifierreader',
                     dashStyle: 'ShortDot',
                     color: colors[5]
                 }, {
                     name: 'Other',
                     data: [42.6, 51.5, 54.2, 45.8, 20.2, 15.4],
-                    website: 'http://www.disabled-world.com/assistivedevices/computer/screen-readers.php',
                     dashStyle: 'ShortDash',
                     color: colors[3]
                 }
